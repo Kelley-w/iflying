3be62f109fad254e5b1e05a72005702257b69d29
keylley-w <15801509633@163.com>
2019-03-02 23:11:56 +0800

    提交

--- iflying/src/router/index.js
+++ iflying/src/router/index.js
@@ -5,8 +5,11 @@ import Destination from '@/components/destination/destination';
 import DestDetails from '@/components/destination/components/dest_details';
 import Plan from '@/components/plan/plan';
 import My from '@/components/my/my';
+import GroupTour from '@/components/home/groupTour/groupTour';
+import Details from '@/components/home/details/details';
 import Login from '@/components/my/components/login'
 import Register from '@/components/my/components/register'
+
 
 
 
@@ -21,8 +24,8 @@ export default new Router({
     {
       path: '/home',
       name: 'home',
-      component: Home
-//    conponent:(resolve)=>require("../components/home/home",resolve)
+      component: Home,
+      
     },
     {
       path: '/destination',
@@ -39,6 +42,17 @@ export default new Router({
       name: 'my',
       component: My
     },
+    //home中组团游的页面
+    {
+    	path:'/groupTour',
+    	name: 'groupTour',
+    	component:GroupTour	
+    },
+    {
+    	path:'/details',
+    	name: 'details',
+    	component:Details	
+    },
     {
     	path:"/login",
     	name:'login',
@@ -53,6 +67,8 @@ export default new Router({
     	path:'/destDetails',
     	name:'destDetails',
     	component:DestDetails
+
     }
-  ]
+    
+ ]
 })
